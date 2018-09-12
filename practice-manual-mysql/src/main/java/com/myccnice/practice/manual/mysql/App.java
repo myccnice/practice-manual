68aeb6295a1c9d4712db1a0e7bb16f2983a725db
wangpeng <wangpeng@51web.com>
2018-09-12 11:17:01 +0800

    新增数据库模块

---
+++ practice-manual-mysql/src/main/java/com/myccnice/practice/manual/mysql/App.java
@@ -0,0 +1,7 @@
+package com.myccnice.practice.manual.mysql;
+
+public class App  {
+    public static void main( String[] args ) {
+
+    }
+}
---
+++ practice-manual-mysql/src/test/java/com/myccnice/practice/manual/mysql/AppTest.java
@@ -0,0 +1,5 @@
+package com.myccnice.practice.manual.mysql;
+
+public class AppTest  {
+    
+}
